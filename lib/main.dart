0c51df0a212ac682e118b8b8ad7d39ccea274b66
Mateo <mateobencek007@gmail.com>
2020-12-28 15:08:33 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+// Import MaterialApp and other widgets which we can use to quickly create a material app
+import 'package:flutter/material.dart';
+
+// Code written in Dart starts exectuting from the main function. runApp is part of
+// Flutter, and requires the component which will be our app's container. In Flutter,
+// every component is known as a "widget".
+void main() => runApp(new TodoApp());
+
+// Every component in Flutter is a widget, even the whole app itself
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Todo List',
+      home: new ToDoList() 
+    );
+  }
+}
+
+class ToDoList extends StatefulWidget {
+  @override
+  createState() => new ToDoListState();
+}
+
+class ToDoListState extends State<ToDoList> {
+  List<String> _todoItems = [];
+
+  // This will be called each time + is pressed
+  void _addTodoItem() {
+    // Putting our code inside "setState" tells the app that our state has changed, and
+    // it will automatically re-render the list
+    setState(() {
+      int index = _todoItems.length;
+      _todoItems.add('Item ' + index.toString());
+    });
+  }
+
+  // Build the whole list of todo items
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        // itemBuilder will be automatically be called as many times as it takes for the
+        // list to fill up its available space, which is most likely more than the
+        // number of todo items we have. So, we need to check the index is OK.
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index]);
+        }
+      },
+    );
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText) {
+    return new ListTile(
+      title: new Text(todoText)
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Todo List")
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _addTodoItem,
+        tooltip: 'Add task',
+        child: new Icon(Icons.add)
+      ),
+    );
+  }
+}
